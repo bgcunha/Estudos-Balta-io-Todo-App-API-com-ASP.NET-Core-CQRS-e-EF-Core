035f370abba75ab3f5ef80f077601c19569c9ef6
Bruno Expedito Gomes da Cunha <bruno.gomes.da.cunha@avanade.com>
2022-02-11 15:36:00 -0300

    definindo commandResult

---
+++ Todo.Domain/Commands/Contracts/ICommandResult.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Todo.Domain.Commands.Contracts;
+public interface ICommandResult
+{
+    public bool Success { get; set; }
+    public string Message { get; set; }
+    public object Data { get; set; }
+}
