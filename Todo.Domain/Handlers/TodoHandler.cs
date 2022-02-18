39bc3e2552848af5017e7af23baa82b90b748056
Bruno Expedito Gomes da Cunha <bruno.gomes.da.cunha@avanade.com>
2022-02-18 16:59:44 -0300

    add handler part1

---
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -0,0 +1,25 @@
+﻿using Flunt.Notifications;
+using Todo.Domain.Commands;
+using Todo.Domain.Commands.Contracts;
+using Todo.Domain.Handlres.Contracts;
+using Todo.Domain.Repositories;
+
+namespace Todo.Domain.Handlers;
+public class TodoHandler :
+    Notifiable,
+    IHandler<CreateTodoCommand>
+{
+
+    private readonly ITodoRepository _repository;
+
+    public TodoHandler(ITodoRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public ICommandResult Handle(CreateTodoCommand command)
+    {
+        throw new NotImplementedException();
+    }
+}
+
