3bc96e368b118fa02df5c6a4e85e993a4d362089
Bruno Gomes <50124660+bgcunha@users.noreply.github.com>
2022-02-21 21:44:22 -0300

    green e refactor

--- Todo.Domain.Tests/CommandTests/CreateTodoCommandTests.cs
+++ Todo.Domain.Tests/CommandTests/CreateTodoCommandTests.cs
@@ -1,20 +1,32 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Todo.Domain.Commands;
 
 namespace Todo.Domain.Tests.CommandTests
 {
     [TestClass]
     public class CreateTodoCommandTests
     {
+       private readonly CreateTodoCommand _Invalidcommand = new CreateTodoCommand("", "", DateTime.Now);
+       private readonly CreateTodoCommand _Validcommand = new CreateTodoCommand("Ir ao mercado", "Bruno Gomes", DateTime.Now);
+
+        public CreateTodoCommandTests()
+        {
+            _Invalidcommand.Validate();
+            _Validcommand.Validate();
+
+        }
+
         [TestMethod]
         public void Dado_um_comando_invalido()
         {
-            Assert.Fail();
+            Assert.AreEqual(_Invalidcommand.Valid, false);
         }
 
         [TestMethod]
         public void Dado_um_comando_valido()
         {
-            Assert.Fail();
+            Assert.AreEqual(_Validcommand.Valid, true);
         }
     }
 }
