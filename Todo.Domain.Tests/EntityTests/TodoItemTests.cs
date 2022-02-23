2821a442d34ee277261ca3ac3f59db816390d9a9
Bruno Gomes <50124660+bgcunha@users.noreply.github.com>
2022-02-22 22:39:27 -0300

    testando a entidade

---
+++ Todo.Domain.Tests/EntityTests/TodoItemTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Tests.EntityTests
+{
+    [TestClass]
+    public class TodoItemTests
+    {
+        private readonly TodoItem _todo = new TodoItem("Fazer meditação", DateTime.Now, "Bruno Gomes");
+
+        [TestMethod]
+        public void Dado_um_novo_todo_o_mesmo_nao_pode_ser_concluido()
+        {
+            Assert.AreEqual(_todo.Done, false);
+        }
+    }
+}
